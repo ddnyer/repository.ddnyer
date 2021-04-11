e6654a039ae18dbfb869b195c613d8398cee3c2f
Damien <damien.denayer@gmail.com>
2021-04-11 23:34:15 +0200

    add turn off tv script

---
+++ script.power.off/default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import xbmc, xbmcaddon, time
+
+ADDON = xbmcaddon.Addon()
+ADDON_ID = ADDON.getAddonInfo('id')
+ADDON_NAME = ADDON.getAddonInfo('name')
+LOG_MESSAGE = ADDON.getAddonInfo('name') + ': {}'
+
+
+def log(message):
+    xbmc.log(LOG_MESSAGE.format(message), xbmc.INFO)
+
+
+log("Turn off the TV")
+
+if xbmc.getCondVisibility("Player.HasMedia"):
+    log("Stop player first")
+    xbmc.executebuiltin("PlayerControl(Stop)")
+    time.sleep(1)
+
+xbmc.executebuiltin("ActivateScreensaver")
